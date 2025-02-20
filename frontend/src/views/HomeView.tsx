098b89c0c614665cb66be297a6580e4ae6f4fa1c
Abhi Mitra <apon555@gmail.com>
2025-02-20 21:18:16 +0530

    first commit

---
+++ frontend/src/views/HomeView.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const HomeView: FC = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-8">Home Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomeView;
